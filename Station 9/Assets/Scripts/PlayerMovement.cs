cf4b9532995fcbdd6cad6e180267efdd8dc68d58
grant <gmacmil3@my.centennialcollege.ca>
2022-04-20 03:37:30 -0400

    Added Working Terminal

--- Station 9/Assets/Scripts/PlayerMovement.cs
+++ Station 9/Assets/Scripts/PlayerMovement.cs
@@ -7,6 +7,8 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public Transform cam;
 
+    public static bool canMove = true;
+
     public float speed = 10.0f;
 
     public float turnSmoothTime = 0.1f;
@@ -20,19 +22,23 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float z = Input.GetAxisRaw("Vertical");
-        Vector3 direction = new Vector3(x,0.0f, z).normalized; //dont allow player to move vertically, also .normalized will make it so moving diaginonal dosent make u move faster :)
-
-        if (direction.magnitude >= 0.1f)
+        if (canMove == true)
         {
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
-            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+            float x = Input.GetAxisRaw("Horizontal");
+            float z = Input.GetAxisRaw("Vertical");
+            Vector3 direction = new Vector3(x, 0.0f, z).normalized; //dont allow player to move vertically, also .normalized will make it so moving diaginonal dosent make u move faster :)
 
-            Vector3 movementDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
-            controller.Move(movementDirection.normalized * speed * Time.deltaTime);
+            if (direction.magnitude >= 0.1f)
+            {
+                float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
+                transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
+                Vector3 movementDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+                controller.Move(movementDirection.normalized * speed * Time.deltaTime);
+            }
         }
+        
 
         
     }
---
+++ Station 9/Assets/Scripts/Terminal.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Terminal : MonoBehaviour
+{
+    public static bool terminalIsOpen = false; //not open by default
+    public bool inRange = false;
+
+    public GameObject door;
+
+    public GameObject terminalUI; //UI pannel that we want to open
+    public GameObject interactPrompt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && inRange == true) 
+        {
+            OpenTerminal();
+        }
+    }
+
+    public void OpenTerminal()
+    {
+        interactPrompt.SetActive(false);
+        terminalUI.SetActive(true); //sets terminal visable to player in game view
+        terminalIsOpen = true;
+        Cursor.lockState = CursorLockMode.None;
+        PlayerMovement.canMove = false;
+    }
+
+    public void CloseTerminal()
+    {
+        interactPrompt.SetActive(true);
+        terminalUI.SetActive(false); //sets terminal NOT visable to player in game view
+        terminalIsOpen = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        PlayerMovement.canMove = true;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRange = true;
+            interactPrompt.SetActive(true);
+        }
+        
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            inRange = false;
+            interactPrompt.SetActive(false);
+        }
+    }
+
+    public void OpenDoor()
+    {
+        door.SetActive(false);
+    }
+
+    public void CloseDoor()
+    {
+        door.SetActive(true);
+    }
+
+}
