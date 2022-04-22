ae61900d0652185a9a67f0989c0cb7816d2b2e94
grant <gmacmil3@my.centennialcollege.ca>
2022-04-22 17:17:52 -0400

    Changes to Player Prefab

--- Station 9/Assets/Scripts/PlayerMovement.cs
+++ Station 9/Assets/Scripts/PlayerMovement.cs
@@ -6,36 +6,59 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public Transform cam;
-
     public static bool canMove = true;
 
     public float speed = 10.0f;
+    public float gravity = -45f;
+    public float jumpHeight = 1.5f;
+    Vector3 velocity;
+    bool isGrounded;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
 
     public float turnSmoothTime = 0.1f;
     public float turnSmoothVel;
 
     private void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.lockState = CursorLockMode.Locked; //lock cursor to screen
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (canMove == true)
+        if (canMove == true) //only move if player is allowed to
         {
+            //jump
+            isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+            if (isGrounded && velocity.y < 0)
+            {
+                velocity.y = -2f;
+            }
+
+            if (Input.GetButtonDown("Jump") && isGrounded)
+            {
+                velocity.y = Mathf.Sqrt(jumpHeight * -2 * gravity);
+            }
+            //gravity
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+            //walk
             float x = Input.GetAxisRaw("Horizontal");
             float z = Input.GetAxisRaw("Vertical");
-            Vector3 direction = new Vector3(x, 0.0f, z).normalized; //dont allow player to move vertically, also .normalized will make it so moving diaginonal dosent make u move faster :)
+            Vector3 direction = new Vector3(x, 0f, z).normalized;
 
             if (direction.magnitude >= 0.1f)
             {
                 float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
                 float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVel, turnSmoothTime);
-                transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+                transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-                Vector3 movementDirection = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
-                controller.Move(movementDirection.normalized * speed * Time.deltaTime);
+                Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+                controller.Move(moveDir.normalized * speed * Time.deltaTime);
             }
         }
         
