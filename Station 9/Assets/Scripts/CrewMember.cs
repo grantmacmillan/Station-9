3705f1c7848f789bf84b76e2288852626ff35568
grant <gmacmil3@my.centennialcollege.ca>
2022-05-03 20:32:28 -0400

    Added crewmate AI

--- Station 9/Assets/Scripts/CrewMember.cs
+++ Station 9/Assets/Scripts/CrewMember.cs
@@ -2,27 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CrewMember : MonoBehaviour
 {
+    public NavMeshAgent agent;
+    public Transform rescuePoint;
+
+    public LayerMask whatIsGround;
+
+    //wandering
+    public Vector3 walkPoint;
+    bool walkPointSet;
+    public float walkPointRange;
+
+    //rescue
+    public bool beingRescued = false;
+
     public bool inRange = false;
     public GameObject interactPrompt;
 
     public int crewMembersSaved = 0;
     public TextMeshProUGUI savedCounterText;
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        rescuePoint = GameObject.Find("RescuePoint").transform; //find the player in the scene
+        agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!beingRescued)
+        {
+            Wandering();
+        }
+
         if (Input.GetKeyDown(KeyCode.E) && inRange == true)
         {
             Rescue();
         }
+
+        
     }
 
     void OnTriggerEnter(Collider other)
@@ -43,11 +64,48 @@ public class CrewMember : MonoBehaviour
         }
     }
 
+    
+
+    private void Wandering()
+    {
+        if (!walkPointSet)
+        {
+            SearchWalkPoint();
+        }
+
+        if (walkPointSet)
+        {
+            agent.SetDestination(walkPoint);
+        }
+
+        Vector3 distanceToWalkPoint = transform.position - walkPoint;
+
+        //walkpoint reached
+        if (distanceToWalkPoint.magnitude < 1.0f)
+        {
+            walkPointSet = false;
+        }
+    }
+    private void SearchWalkPoint()
+    {
+        //make a random point that is in the range
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+
+        walkPoint = new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
+
+        if (Physics.Raycast(walkPoint, -transform.up, 2.0f, whatIsGround))
+        {
+            walkPointSet = true;
+        }
+    }
+
     public void Rescue()
     {
+        beingRescued = true;
         PlayerMovement.crewMembersRescued++;
         savedCounterText.text = "Crew Saved: " + PlayerMovement.crewMembersRescued;
-        Destroy(gameObject);
+        agent.SetDestination(rescuePoint.position);
         interactPrompt.SetActive(false);
     }
 }
